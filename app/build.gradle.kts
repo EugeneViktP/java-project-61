892996df6215502b0855858f0e72581d7c4cd518
Eugene Popov <popoff.zhenya@yandex.ru>
2023-12-09 15:46:45 +0300

    added new project app

---
+++ app/build.gradle.kts
@@ -0,0 +1,43 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
+plugins {
+    application
+    jacoco
+    id("checkstyle")
+    id("java")
+    id ("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("io.freefair.lombok") version "8.4"
+    id ("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    // testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    // testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
+    }
+}
+
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
