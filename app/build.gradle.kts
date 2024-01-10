d310521600165e10e7d9fca6f1d11b6f9bc17116
Eugene Popov <popoff.zhenya@yandex.ru>
2024-01-10 17:49:04 +0300

    new update

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,42 +1,24 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.gradle.api.tasks.testing.logging.TestLogEvent
-
 plugins {
     application
-    jacoco
-    id ("checkstyle")
+    checkstyle
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
+application { mainClass.set("hexlet.code.App") }
 
-application {mainClass.set("hexlet.code.App")}
-
-repositories {
-    mavenCentral()
-}
+repositories { mavenCentral() }
 
 dependencies {
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    implementation("org.apache.commons:commons-collections4:4.4")
-    implementation("org.apache.commons:commons-lang3:3.14.0")
-    testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
-}
 
-tasks.test {
-    useJUnitPlatform()
-    testLogging {
-        exceptionFormat = TestExceptionFormat.FULL
-        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
-        // showStackTraces = true
-        // showCauses = true
-        showStandardStreams = true
+}
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(20))
     }
 }
 
-tasks.jacocoTestReport { reports { xml.required.set(true) } }
-
-tasks.getByName("run",JavaExec::class) {
+val run by tasks.getting(JavaExec::class) {
     standardInput = System.`in`
 }
