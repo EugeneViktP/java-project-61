9f7d6d1b36d989cdfa9d58bd168cc9a0182a74d4
Eugene Popov <popoff.zhenya@yandex.ru>
2024-01-10 15:19:19 +0300

    returned old file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     application
     jacoco
     id ("checkstyle")
+    id ("se.patrikerdes.use-latest-versions") version "0.2.18"
     id("io.freefair.lombok") version "8.4"
     id ("com.github.ben-manes.versions") version "0.50.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
@@ -22,8 +23,9 @@ repositories {
 dependencies {
     implementation("org.apache.commons:commons-lang3:3.14.0")
     implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
     testImplementation(platform("org.junit:junit-bom:5.10.1"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
 }
 
 tasks.test {
@@ -38,3 +40,7 @@ tasks.test {
 }
 
 tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+tasks.getByName("run",JavaExec::class) {
+    standardInput = System.`in`
+}
