cc35a058755b1e1cd9d79c380a405aa9acc53c8c
Eugene Popov <popoff.zhenya@yandex.ru>
2024-01-10 19:24:25 +0300

    file

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,24 +1,49 @@
 import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+
 plugins {
     application
-    checkstyle
+    jacoco
+    id ("checkstyle")
+    id("java")
+    id ("se.patrikerdes.use-latest-versions") version "0.2.18"
+    id("io.freefair.lombok") version "8.4"
+    id ("com.github.ben-manes.versions") version "0.50.0"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 group = "hexlet.code"
 version = "1.0-SNAPSHOT"
-application { mainClass.set("hexlet.code.App") }
 
-repositories { mavenCentral() }
+application {mainClass.set("hexlet.code.App")}
+
+repositories {
+    mavenCentral()
+}
 
 dependencies {
-
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    implementation("org.apache.commons:commons-collections4:4.4")
+    implementation("org.apache.commons:commons-lang3:3.14.0")
+    testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter:5.9.2")
+    // testImplementation(platform("org.junit:junit-bom:5.10.1"))
+    // testImplementation("org.junit.jupiter:junit-jupiter")
 }
-java {
-    toolchain {
-        languageVersion.set(JavaLanguageVersion.of(20))
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        exceptionFormat = TestExceptionFormat.FULL
+        events = mutableSetOf(TestLogEvent.FAILED, TestLogEvent.PASSED, TestLogEvent.SKIPPED)
+        // showStackTraces = true
+        // showCauses = true
+        showStandardStreams = true
     }
 }
 
-val run by tasks.getting(JavaExec::class) {
+tasks.jacocoTestReport { reports { xml.required.set(true) } }
+
+tasks.getByName("run",JavaExec::class) {
     standardInput = System.`in`
 }
