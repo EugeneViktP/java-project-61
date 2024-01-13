6b713e78bcb7f959b1ca4a1b5550b74a03cac29a
Eugene Popov <popoff.zhenya@yandex.ru>
2024-01-13 15:56:30 +0300

    updated Prime game

--- app/src/main/java/hexlet/code/games/Prime.java
+++ app/src/main/java/hexlet/code/games/Prime.java
@@ -6,10 +6,10 @@ public class Prime {
     public static final String PRIME_RULES = "Answer 'yes' if given number is prime. Otherwise answer 'no'.";
 
     public static boolean isPrime(int number) {
-        if (number < 2) {
+        if (number <= 1) {
             return false;
         } else {
-            for (var i = 2; i < number; i++) {
+            for (var i = 2; i < number / 2; i++) {
                 if (number % i == 0) {
                     return  false;
                 }
